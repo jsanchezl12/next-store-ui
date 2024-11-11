14c90ab396504f12284b0565f886149e91b19ab4
Juan Sebastian Sanchez L. <js.sanchezl12@uniandes.edu.co>
2024-11-11 18:38:22 -0500

    :rocket: update files for deploy in cloudflare

--- src/app/models/types.ts
+++ src/app/models/types.ts
@@ -1,6 +1,72 @@
-// types.ts
-export interface IProduct {
+// src/types/Product.ts
+export interface Variant {
+  id: number;
+  product_id: number;
+  title: string;
+  price: string;
+  position: number;
+  inventory_policy: string;
+  compare_at_price: string | null;
+  option1: string;
+  option2: string | null;
+  option3: string | null;
+  created_at: string;
+  updated_at: string;
+  taxable: boolean;
+  barcode: string | null;
+  fulfillment_service: string;
+  grams: number;
+  inventory_management: string;
+  requires_shipping: boolean;
+  sku: string | null;
+  weight: number;
+  weight_unit: string;
+  inventory_item_id: number;
+  inventory_quantity: number;
+  old_inventory_quantity: number;
+  admin_graphql_api_id: string;
+  image_id: number | null;
+}
+
+export interface Option {
+  id: number;
+  product_id: number;
+  name: string;
+  position: number;
+  values: string[];
+}
+
+export interface Image {
+  id: number;
+  alt: string | null;
+  position: number;
+  product_id: number;
+  created_at: string;
+  updated_at: string;
+  admin_graphql_api_id: string;
+  width: number;
+  height: number;
+  src: string;
+  variant_ids: number[];
+}
+
+export interface Product {
   id: number;
   title: string;
-  // Add other relevant fields here
+  body_html: string;
+  vendor: string;
+  product_type: string;
+  created_at: string;
+  handle: string;
+  updated_at: string;
+  published_at: string;
+  template_suffix: string;
+  published_scope: string;
+  tags: string;
+  status: string;
+  admin_graphql_api_id: string;
+  variants: Variant[];
+  options: Option[];
+  images: Image[];
+  image: Image | null;
 }
